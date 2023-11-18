edf71bafa1a8f08c6334458c35a5bffa4f0b70e6
Horlew-myde <mezamune@gmail.com>
2023-11-18 12:09:16 +0100

    add signout func

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { google, signin, signup } from '../controller/auth.cotroller.js';
+import { google, signOut, signin, signup } from '../controller/auth.cotroller.js';
 
 const router = express.Router();
 
 router.post('/signup', signup);
 router.post('/signin', signin);
-router.post('/google', google)
+router.post('/google', google);
+router.get('/signout', signOut);
 
 
 export default router;
