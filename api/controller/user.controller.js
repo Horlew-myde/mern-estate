04c27bc47d8e126949e79f2d2a2bb31d635afeb1
Horlew-myde <mezamune@gmail.com>
2023-11-20 18:42:25 +0100

    added contact landlord functionality to listing p

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -5,13 +5,13 @@ import Listing from '../models/listing.model.js';
 
 export const test = (req, res) => {
   res.json({
-    message: "Api Route is Working",
+    message: 'Api route is working!',
   });
 };
 
 export const updateUser = async (req, res, next) => {
   if (req.user.id !== req.params.id)
-    return next(errorHandler(401, "You can only update your own account!"));
+    return next(errorHandler(401, 'You can only update your own account!'));
   try {
     if (req.body.password) {
       req.body.password = bcryptjs.hashSync(req.body.password, 10);
@@ -40,11 +40,11 @@ export const updateUser = async (req, res, next) => {
 
 export const deleteUser = async (req, res, next) => {
   if (req.user.id !== req.params.id)
-    return next(errorHandler(401, "You can only delete your own account!"));
+    return next(errorHandler(401, 'You can only delete your own account!'));
   try {
     await User.findByIdAndDelete(req.params.id);
-    res.clearCookie("access_token");
-    res.status(200).json("User has been deleted!");
+    res.clearCookie('access_token');
+    res.status(200).json('User has been deleted!');
   } catch (error) {
     next(error);
   }
@@ -59,20 +59,21 @@ export const getUserListings = async (req, res, next) => {
       next(error);
     }
   } else {
-    return next(errorHandler(401, "You can only view your own listings!"));
+    return next(errorHandler(401, 'You can only view your own listings!'));
   }
 };
 
-// export const getUser = async (req, res, next) => {
-//   try {
-//     const user = await User.findById(req.params.id);
-
-//     if (!user) return next(errorHandler(404, "User not found!"));
-
-//     const { password: pass, ...rest } = user._doc;
-
-//     res.status(200).json(rest);
-//   } catch (error) {
-//     next(error);
-//   }
-// };
+export const getUser = async (req, res, next) => {
+  try {
+    
+    const user = await User.findById(req.params.id);
+  
+    if (!user) return next(errorHandler(404, 'User not found!'));
+  
+    const { password: pass, ...rest } = user._doc;
+  
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
